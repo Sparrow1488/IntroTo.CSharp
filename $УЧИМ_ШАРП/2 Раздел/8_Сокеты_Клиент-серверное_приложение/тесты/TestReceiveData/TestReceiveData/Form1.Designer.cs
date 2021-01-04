552b08b5d1ca12b0153e219d1d66b63204978b4b
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-04 21:09:56 +0300

    начал изучать WPF, понял принып работы делегатов в сокетах и формах

---
+++ $УЧИМ_ШАРП/2 Раздел/8_Сокеты_Клиент-серверное_приложение/тесты/TestReceiveData/TestReceiveData/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TestReceiveData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(162, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(347, 184);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 206);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ $УЧИМ_ШАРП/2 Раздел/8_Сокеты_Клиент-серверное_приложение/тесты/TestReceiveData/TestReceiveData/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestReceiveData
+{
+    public partial class Form1 : Form
+    {
+        private static UdpClient myClient = new UdpClient(8080);//SEND DATA ON THIS PORT
+        private string getData = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void recv(IAsyncResult res)
+        {
+            IPEndPoint remoteIP = new IPEndPoint(IPAddress.Any, 9000);
+            byte[] received = myClient.EndReceive(res, ref remoteIP);
+
+            getData = Encoding.ASCII.GetString(received);
+
+
+            //MethodInvoket - делегат, в котором мы можем просто указать любой метода / действие
+            Invoke(new MethodInvoker(delegate
+            {
+                richTextBox1.Text += $"\nReceived data: {getData}";
+            }));
+            myClient.BeginReceive(new AsyncCallback(recv), null); //AsyncCallback - делегат, ссылается на метод, который должен вызываться при завершении соответствующей асинхронной операции.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                myClient.BeginReceive(new AsyncCallback(recv), null);
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += $"\n{ex.Message}";
+            }
+        }
+    }
+}
