676a0eebcee86902a8abf983f47c7ce745de00b0
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-09 23:06:45 +0300

    игра cock

---
+++ $НАРАБОТКИ/РАБОЧИЕ/игра помойка/MainGame/MainGame/GamePanel.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace MainGame
+{
+    partial class GamePanel
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GamePanel));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 170);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(0, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "СДЕЛАТЬ КРУТО";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(51, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 11);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "click1";
+            this.label1.MouseLeave += new System.EventHandler(this.label3_MouseLeave_1);
+            this.label1.MouseHover += new System.EventHandler(this.labelCockInfo_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(114, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 11);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "click2";
+            this.label2.MouseLeave += new System.EventHandler(this.label3_MouseLeave_1);
+            this.label2.MouseHover += new System.EventHandler(this.labelCockInfo_MouseHover);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(81, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 9);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "click3";
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave_1);
+            this.label3.MouseHover += new System.EventHandler(this.labelCockInfo_MouseHover);
+            // 
+            // GamePanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(186, 255);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GamePanel";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ $НАРАБОТКИ/РАБОЧИЕ/игра помойка/MainGame/MainGame/GamePanel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MainGame
+{
+    public partial class GamePanel : Form
+    {
+        public GamePanel()
+        {
+            InitializeComponent();
+        }
+
+        private void labelCockInfo_MouseHover(object sender, EventArgs e)
+        {
+            label1.BackColor = GetRandomColor();
+            label2.BackColor = GetRandomColor();
+            label3.BackColor = GetRandomColor();
+        }
+        private void label3_MouseLeave_1(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            label3.BackColor = Color.Transparent;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.ForeColor = GetRandomColor();
+        }
+
+        List<Color> colors = new List<Color>() { Color.BlueViolet, Color.Aqua, Color.Brown, Color.AliceBlue, Color.Olive, Color.Red };
+        Color lastColor = Color.Black;
+        private Color GetRandomColor()
+        {
+            Random rndColor = new Random();
+            Color colorActive = colors[rndColor.Next(0, colors.Count)];
+            while (true)
+            {
+                if (colorActive.Equals(lastColor))
+                {
+                    colorActive = colors[rndColor.Next(0, colors.Count)];
+                }
+                if (!colorActive.Equals(lastColor))
+                {
+                    lastColor = colorActive;
+                    break;
+                }
+            }
+            return colorActive;
+        }
+    }
+}
