51e86a1764311f21db90a8ed90446c3b271d2b4c
Sparrow1488 <ilyaokubev@gmail.com>
2021-01-16 23:00:39 +0300

    сортир

--- $НАРАБОТКИ/НЕ РАБОЧИЕ/DataBaseTEST/DataBaseWork/DataBaseWork/Form1.cs
+++ $НАРАБОТКИ/НЕ РАБОЧИЕ/DataBaseTEST/DataBaseWork/DataBaseWork/Form1.cs
@@ -4,12 +4,6 @@
 using System.Data;
 using System.Data.SqlClient;
 
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DataBaseWork
