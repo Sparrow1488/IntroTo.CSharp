b2c04266b1b4d0fdef1de54a75e5dbdc1749aa45
Sparrow1488 <ilyaokubev@gmail.com>
2021-02-12 19:34:19 +0300

    начал работать с LINQ

---
+++ $XAML/1_Основа/4_PAGES/WindowWithPages/WindowWithPages/MainAppWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WindowWithPages.Pages_in_app;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowWithPages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainAppWindow.xaml
+    /// </summary>
+    public partial class MainAppWindow : Window
+    {
+        public MainAppWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainContentFrame.Content = new Main_page();
+        }
+    }
+}
