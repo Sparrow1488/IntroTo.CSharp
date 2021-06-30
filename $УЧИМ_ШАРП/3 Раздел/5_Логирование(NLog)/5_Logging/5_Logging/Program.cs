312b55b08f52a70d68592c1dfe39cc49128187c0
Sparrow1488 <ilyaokubev@gmail.com>
2021-06-30 12:30:21 +0300

    изучаю логирование

---
+++ $УЧИМ_ШАРП/3 Раздел/5_Логирование(NLog)/5_Logging/5_Logging/Program.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using System;
+
+namespace _5_Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = LogManager.GetCurrentClassLogger();
+            // нужна настройка
+        }
+    }
+}
