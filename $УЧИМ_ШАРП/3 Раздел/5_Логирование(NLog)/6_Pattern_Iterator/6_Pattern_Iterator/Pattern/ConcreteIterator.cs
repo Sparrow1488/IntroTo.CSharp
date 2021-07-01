ea69d7aba0fd02274ce2849a6bd6072ccdc79047
Sparrow1488 <ilyaokubev@gmail.com>
2021-07-01 15:48:23 +0300

    изучаю получается паттерн Iterator

---
+++ $УЧИМ_ШАРП/3 Раздел/5_Логирование(NLog)/6_Pattern_Iterator/6_Pattern_Iterator/Pattern/ConcreteIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_Pattern_Iterator.Pattern
+{
+    public class ConcreteIterator : Iterator
+    {
+        private ConcreteAggreagate _concreteAggregate;
+        private int current;
+        public ConcreteIterator(ConcreteAggreagate aggregate) : base(aggregate)
+        {
+            _concreteAggregate = aggregate;
+        }
+
+        public override object First()
+        {
+            return _concreteAggregate[0];
+        }
+
+        public override object GetCurrent()
+        {
+            return _concreteAggregate[current];
+        }
+
+        public override bool IsDone()
+        {
+            return current >= _concreteAggregate.Count-1;
+        }
+
+        public override object Next()
+        {
+            object item = null;
+            if (current < _concreteAggregate.Count - 1)
+                item = _concreteAggregate[++current];
+            return item;
+        }
+    }
+}
