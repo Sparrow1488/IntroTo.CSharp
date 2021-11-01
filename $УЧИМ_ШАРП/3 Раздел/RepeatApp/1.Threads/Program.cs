2f5d39ab33167c9fac5155ff763ebb1c17ab62d5
Валентин <ilyaokubev@gmail.com>
2021-11-01 11:39:02 +0300

    изучаю потоки. Метод JOIN

---
+++ $УЧИМ_ШАРП/3 Раздел/RepeatApp/1.Threads/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace _1.Threads
+{
+    public class Program
+    {
+        private static Thread _first;
+        private static Thread _second;
+        public static void Main(string[] args)
+        {
+            _first = new Thread(Foo2);
+            _first.Name = "FIRST";
+            _first.Start();
+
+            _second = new Thread(Foo);
+            _second.Name = "SECOND";
+            _second.Start();
+
+            int max = 500;
+
+            Console.ReadKey();
+        }
+        private static void Foo()
+        {
+            int max = 25;
+            _first.Join();
+            for (int i = 0; i < max; i++)
+                PrintThreadInfo();
+        }
+
+        private static void Foo2()
+        {
+            Console.WriteLine($"[{Thread.CurrentThread.Name}]Foo. ");
+            Console.WriteLine($"[{Thread.CurrentThread.Name}]Foo start Sleep");
+            PrintThreadInfo();
+            Thread.Sleep(5000);
+            Console.WriteLine($"[{Thread.CurrentThread.Name}]Foo was Sleeped");
+        }
+        private static void PrintThreadInfo()
+        {
+            Console.WriteLine($"\n[{_first.Name}]{_first.ThreadState}");
+            Console.WriteLine($"[{_second.Name}]{_second.ThreadState}");
+        }
+
+    }
+}
---
+++ $УЧИМ_ШАРП/3 Раздел/RepeatApp/RepeatApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace RepeatApp
+{
+    public class Program
+    {
+        private static Thread _second;
+        private static Thread _current;
+        public static void Main(string[] args)
+        {
+            _current = Thread.CurrentThread;
+            _second = new Thread(Foo);
+            _second.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                i++;
+                Console.WriteLine("Main. ", i);
+
+                if (i == 9)
+                    Console.WriteLine("Main Finish. ", i);
+            }
+        }
+        private static void Foo()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                i++;
+                Console.WriteLine("Foo. ", i);
+
+
+                if (i == 9)
+                    Console.WriteLine("Foo Finish. ", i);
+            }
+        }
+    }
+}
