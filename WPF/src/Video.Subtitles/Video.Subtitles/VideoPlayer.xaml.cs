6c4c675cd12e4413c8d81730e1d3c78be15e9a81
Sparrow1488 <ilyaokubev@gmail.com>
2021-11-06 22:16:23 +0300

    делаю адаптивный плеер с поддержкой субтитров + по фану практикуюсь делать адаптивность в WPF

---
+++ WPF/src/Video.Subtitles/Video.Subtitles/VideoPlayer.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Video.Subtitles
+{
+    public partial class VideoPlayer : UserControl
+    {
+        public VideoPlayer()
+        {
+            InitializeComponent();
+            Wrapper.MouseEnter += OnMouseEnter;
+            Wrapper.MouseLeave += Player_MouseLeave; ;
+        }
+
+        private void Player_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PlayerHandlePanel.Opacity = 0;
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            PlayerHandlePanel.Opacity = 1;
+        }
+    }
+}
