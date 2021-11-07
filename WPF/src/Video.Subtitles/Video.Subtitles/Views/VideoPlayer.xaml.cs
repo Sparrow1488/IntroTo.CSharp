3ea186f0811b2949fefa5bb2dc2cb8e8d99a07bb
Sparrow1488 <ilyaokubev@gmail.com>
2021-11-07 14:14:29 +0300

    накинул анимации для плеера

--- WPF/src/Video.Subtitles/Video.Subtitles/Views/VideoPlayer.xaml.cs
+++ WPF/src/Video.Subtitles/Video.Subtitles/Views/VideoPlayer.xaml.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 
 namespace Video.Subtitles.Views
 {
     public partial class VideoPlayer : UserControl
     {
-        private string _subtitlesText;
         private List<Subtitles> _subtitlesCollection;
         private DispatcherTimer _timer;
 
@@ -20,7 +21,6 @@ namespace Video.Subtitles.Views
             PlayerHandlePanel.MaxHeight = 0;
             Wrapper.MouseEnter += OnMouseEnter;
             Wrapper.MouseLeave += Player_MouseLeave;
-            SubtitlesText.Opacity = 0;
             _subtitlesCollection = new List<Subtitles>
             {
                 new Subtitles{ Text = "Добрый день дамусы и настрадамусы" , Position = new TimeSpan(0, 0, 1) },
@@ -28,25 +28,39 @@ namespace Video.Subtitles.Views
                 new Subtitles{ Text = "А также затронем тему теорией вероятности" , Position = new TimeSpan(0, 0, 5) },
                 new Subtitles{ Text = "s4" , Position = new TimeSpan(0, 0, 8) },
                 new Subtitles{ Text = "s5" , Position = new TimeSpan(0, 0, 10) },
+                new Subtitles{ Text = "Вообще то есть еще субтитры, получайте!" , Position = new TimeSpan(0, 0, 12) },
+                new Subtitles{ Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut turpis diam, commodo at varius at, efficitur eget erat." , Position = new TimeSpan(0, 0, 14) },
+                new Subtitles{ Text = "Cras vehicula suscipit erat, non mollis ante semper vitae." , Position = new TimeSpan(0, 0, 16) },
             };
         }
 
         private void Player_MouseLeave(object sender, MouseEventArgs e)
         {
-            PlayerHandlePanel.MaxHeight = 0;
+            HidePlayerHandler();
         }
 
         private void OnMouseEnter(object sender, MouseEventArgs e)
         {
-            PlayerHandlePanel.MaxHeight = 40;
+            DisplayPlayerHandler();
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Player.Play();
-            SubtitlesText.Opacity = 1;
             _timer = CreateTimer();
             _timer.Start();
+        }
+
+        private void DisplayPlayerHandler()
+        {
+            var animation = new DoubleAnimation(40, new Duration(new TimeSpan(0, 0, 0, 0, 250)));
+            PlayerHandlePanel.BeginAnimation(MaxHeightProperty, animation);
+        }
+
+        private void HidePlayerHandler()
+        {
+            var animation = new DoubleAnimation(0, new Duration(new TimeSpan(0, 0, 0, 0, 250)));
+            PlayerHandlePanel.BeginAnimation(MaxHeightProperty, animation);
         }
 
         private int _subCounter = 0;
