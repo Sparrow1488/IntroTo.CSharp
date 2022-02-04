c34f7071e68dba3a8e19431d0b28313bf3dce913
Валентин <ilyaokubev@gmail.com>
2022-02-04 21:15:51 +0300

    Dispose, Finalize, Destructor

---
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.Destructors/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace LearnSharp4.Destructors
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            using (var stream = new MyStream())
+            {
+                stream.WriteInFile();
+                stream.Dispose();
+            }
+            
+        }
+    }
+
+    public class MyStream : IDisposable
+    {
+        private bool _isDisposed;
+
+        ~MyStream()
+        {
+            Console.WriteLine("Finalize");
+            Dispose(false);
+        }
+
+        public void WriteInFile()
+        {
+            Console.WriteLine("Записываем в файл...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Готово");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this); // подавляем Finalize, тк выполнен Dispose
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Disposing");
+                }
+                _isDisposed = true;
+            }
+        }
+        
+    }
+}
