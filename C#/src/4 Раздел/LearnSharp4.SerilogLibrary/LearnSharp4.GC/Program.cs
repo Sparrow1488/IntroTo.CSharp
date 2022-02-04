a0524fff2e7c6c12508ad4b344ceec3428ef1ff4
Валентин <ilyaokubev@gmail.com>
2022-02-04 21:42:45 +0300

    GC

---
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.GC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using GarbageCollector = System.GC;
+
+namespace LearnSharp4.GC
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var info = new FileInfo("./Program.cs");
+            var generation = GarbageCollector.GetGeneration(info);
+            Console.WriteLine("Поколение - " + generation);
+        }
+    }
+}
