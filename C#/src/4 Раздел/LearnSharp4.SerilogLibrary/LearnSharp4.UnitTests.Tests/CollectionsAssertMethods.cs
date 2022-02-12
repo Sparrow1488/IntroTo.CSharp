bd3bf3a444054a5844667b6f4f5d777df3ad64b1
ValentinuS <ilyaokubev@gmail.com>
2022-02-12 17:59:22 +0300

    Feature[C#]: тестирование коллекций

--- C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.UnitTests.Tests/CollectionsAssertMethods.cs
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.UnitTests.Tests/CollectionsAssertMethods.cs
@@ -8,8 +8,7 @@ namespace LearnSharp4.UnitTests.Tests
     {
         private static List<string> _names;
 
-        [ClassInitialize]
-        public static void NamesInitialize()
+        public CollectionsAssertMethods()
         {
             _names = new List<string>()
             {
@@ -26,5 +25,55 @@ namespace LearnSharp4.UnitTests.Tests
         {
             CollectionAssert.AllItemsAreNotNull(_names);
         }
+
+        [TestMethod]
+        public void AllItemsAreEqualTest()
+        {   
+            var employees = new List<string>()
+            {
+                "Иван",
+                "Петр",
+                "Ефим",
+                "Егор",
+                "Степан"
+            };
+
+            // учитывает последовательность в коллекции
+            CollectionAssert.AreEqual(_names, employees, "Not equals");
+        }
+
+        [TestMethod]
+        public void AllItemsAreEquivalentTest()
+        {
+            var employees = new List<string>()
+            {
+                "Петр",
+                "Иван",
+                "Ефим",
+                "Степан",
+                "Егор"
+            };
+
+            CollectionAssert.AreEquivalent(_names, employees, "Not equivalent");
+        }
+
+        [TestMethod]
+        public void ItemsSubsetTest()
+        {
+            var employees = new List<string>()
+            {
+                "Петр",
+                "Иван"
+            };
+
+            CollectionAssert.IsSubsetOf(employees, _names);
+        }
+
+        [TestMethod]
+        public void AllItemsAreUnique()
+        {
+            CollectionAssert.AllItemsAreUnique(_names);
+        }
+
     }
 }
