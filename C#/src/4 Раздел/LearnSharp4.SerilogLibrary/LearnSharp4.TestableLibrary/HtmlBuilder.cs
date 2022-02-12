6bb3d87f46eb14244132ade79e72a2c971fef9be
ValentinuS <ilyaokubev@gmail.com>
2022-02-12 22:10:17 +0300

    Feature[C#]: настройка теста для работы с файловой системой, DeploymentItem

---
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.TestableLibrary/HtmlBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace LearnSharp4.TestableLibrary
+{
+    public class HtmlBuilder
+    {
+        private string _outHtml = string.Empty;
+
+        public void CreateBlockWithText(string text, DateTime date)
+        {
+            var template = File.ReadAllText("./template.txt");
+            template = template.Replace("{text}", text);
+            template = template.Replace("{date}", date.ToString());
+            _outHtml = template;
+        }
+
+        public string Build()
+        {
+            return _outHtml;
+        }
+    }
+}
---
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.UnitTests.Tests/HtmlBuilderTest.cs
@@ -0,0 +1,28 @@
+﻿using LearnSharp4.TestableLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace LearnSharp4.UnitTests.Tests
+{
+    [TestClass]
+    public class HtmlBuilderTest
+    {
+        [DeploymentItem("Files\\template.txt")]
+        [DeploymentItem("Files\\result.txt")]
+        [TestMethod]
+        public void CreateTextBlockTest()
+        {
+            var builder = new HtmlBuilder();
+            string text = "Крутой текст";
+            var date = new DateTime(2022, 2, 12);
+            var expected = File.ReadAllText("./result.txt");
+
+            builder.CreateBlockWithText(text, date);
+            var result = builder.Build();
+
+            Assert.AreEqual(result, expected);
+
+        }
+    }
+}
