22e31bdab0eb98a125495a91ed2b1f5839ef99c3
ValentinuS <ilyaokubev@gmail.com>
2022-02-12 23:34:10 +0300

    Feature[C#]: короче хватит с меня этого вашего MSTest

--- C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.UnitTests.Tests/ContextTests.cs
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.UnitTests.Tests/ContextTests.cs
@@ -1,8 +1,7 @@
 ﻿using LearnSharp4.TestableLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
-using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace LearnSharp4.UnitTests.Tests
 {
@@ -10,6 +9,11 @@ namespace LearnSharp4.UnitTests.Tests
     public class ContextTests
     {
         public TestContext TestContext { get; set; }
+
+        public ContextTests()
+        {
+            Debug.WriteLine(Directory.GetCurrentDirectory());
+        }
 
         [TestMethod]
         public void TestMethod1()
@@ -19,34 +23,34 @@ namespace LearnSharp4.UnitTests.Tests
             TestContext.WriteLine($"{TestContext.TestName}");
         }
 
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
-                    "test-data.xml", 
-                    "User", 
-                    DataAccessMethod.Sequential)]
-        [TestMethod]
-        public void AddUserTests()
-        {
-            string name = TestContext.DataRow["Name"].ToString();
-            string mail = TestContext.DataRow["Mail"].ToString();
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+        //            "test-data.xml",
+        //            "User",
+        //            DataAccessMethod.Sequential)]
+        //[TestMethod]
+        //public void AddUserTests()
+        //{
+        //    string name = TestContext.DataRow["Name"].ToString();
+        //    string mail = TestContext.DataRow["Mail"].ToString();
 
-            new UserManager().AddUser(name, mail);
+        //    new UserManager().AddUser(name, mail);
 
-            Assert.IsTrue(true);
-        }
+        //    Assert.IsTrue(true);
+        //}
 
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
-                    "test-data.xml",
-                    "User",
-                    DataAccessMethod.Sequential)]
-        [TestMethod]
-        public void TryAddUserTests()
-        {
-            string name = TestContext.DataRow["Name"].ToString();
-            string mail = TestContext.DataRow["Mail"].ToString();
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+        //            "test-data.xml",
+        //            "User",
+        //            DataAccessMethod.Sequential)]
+        //[TestMethod]
+        //public void TryAddUserTests()
+        //{
+        //    string name = TestContext.DataRow["Name"].ToString();
+        //    string mail = TestContext.DataRow["Mail"].ToString();
 
-            var result = new UserManager().TryAddUser(name, mail);
+        //    var result = new UserManager().TryAddUser(name, mail);
 
-            Assert.IsTrue(result);
-        }
+        //    Assert.IsTrue(result);
+        //}
     }
 }
