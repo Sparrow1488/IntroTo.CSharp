6b629593ac30cdff75c40c74af0ff6519b7b617a
Валентин <ilyaokubev@gmail.com>
2022-04-23 18:38:49 +0300

    Update

--- C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.Automapper/Program.cs
+++ C#/src/4 Раздел/LearnSharp4.SerilogLibrary/LearnSharp4.Automapper/Program.cs
@@ -8,15 +8,23 @@ namespace LearnSharp4.Automapper
     {
         private static void Main()
         {
-            var mapperConfiguration = new MapperConfiguration(config => 
-                    config.CreateMap<PersonViewModel, Person>()
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.CreateMap<PersonViewModel, Person>()
                           .ForMember(x => x.Address, src => src.MapFrom(y => new Address()
-                                    {
-                                        HomeNumber = y.HomeNumber.ToString(),
-                                        Street = y.Street,
-                                        Town = y.Town
-                                    }))
-                          .ForMember(x => x.Id, src => src.Ignore()));
+                          {
+                              HomeNumber = y.HomeNumber.ToString(),
+                              Street = y.Street,
+                              Town = y.Town
+                          }))
+                          .ForMember(x => x.Id, src => src.Ignore());
+
+                config.CreateMap<Person, PersonViewModel>()
+                          .ForMember(x => x.HomeNumber, src => src.MapFrom(y => y.Address.HomeNumber))
+                          .ForMember(x => x.Street, src => src.MapFrom(y => y.Address.Street))
+                          .ForMember(x => x.Town, src => src.MapFrom(y => y.Address.Town));
+            });
+
             mapperConfiguration.AssertConfigurationIsValid();
             var mapper = new Mapper(mapperConfiguration);
 
@@ -30,6 +38,8 @@ namespace LearnSharp4.Automapper
             };
 
             var person = mapper.Map<Person>(vm);
+
+            vm = mapper.Map<PersonViewModel>(person);
         }
     }
 }
