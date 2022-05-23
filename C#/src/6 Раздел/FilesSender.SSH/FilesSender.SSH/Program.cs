312f49508c0a398428e03ba5486350c62f8fa046
Валентин <ilyaokubev@gmail.com>
2022-05-23 20:54:22 +0300

    Feature: added files sender using ssh

---
+++ C#/src/6 Раздел/FilesSender.SSH/FilesSender.SSH/Program.cs
@@ -0,0 +1,21 @@
+﻿using Renci.SshNet;
+using System.Configuration;
+
+var host = ConfigurationManager.AppSettings.Get("host");
+var username = ConfigurationManager.AppSettings.Get("username");
+var password = ConfigurationManager.AppSettings.Get("password");
+var filePath = ConfigurationManager.AppSettings.Get("filePath")!;
+var fileName = Path.GetFileName(filePath);
+var destinationPath = ConfigurationManager.AppSettings.Get("destination") + "/" + fileName;
+
+using (var client = new ScpClient(host, username, password))
+{
+    client.Connect();
+
+    using (var fileStream = new FileStream(filePath, FileMode.Open))
+    {
+        client.Upload(fileStream, destinationPath);
+    }
+
+    client.Disconnect();
+}
