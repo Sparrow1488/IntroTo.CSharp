5176535aee65a8df3fd3d5a95b34683785483fa3
Sparrow1488 <ilyaokubev@gmail.com>
2022-07-25 16:53:18 +0300

    Update: global revision

--- Practice/НЕ РАБОЧИЕ/TestDB1/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TESTtableDB]
-(
-	[Login] VARCHAR(24) NOT NULL PRIMARY KEY, 
-    [Password] VARCHAR(24) NOT NULL, 
-    [Name] VARCHAR(24) NOT NULL, 
-    [Email] VARCHAR(50) NOT NULL
-)
